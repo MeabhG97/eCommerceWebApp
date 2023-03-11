cf4ad769a5252bf443ffdf96eea9cefca304f22f
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-03-11 17:18:38 +0000

    Username and email validation

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -14,12 +14,36 @@ export default class Register extends Component{
             email: "",
             password: "",
             confirmPassword: "",
-            isRegistered: false
+            isRegistered: false,
+            isNameValid: true,
+            isEmailValid: true
         };
     }
 
     handleInputChange = (e) => {
         this.setState({[e.target.name]: e.target.value});
+    }
+
+    componentDidUpdate(prevState){
+        if(this.state.name !== prevState.name){
+            let namePattern = /^[\w]{4,20}$/;
+            if(!this.state.name.match(namePattern) && this.state.isNameValid){
+                this.setState({isNameValid: false});
+            }
+            else if(this.state.name.match(namePattern) && !this.state.isNameValid){
+                this.setState({isNameValid: true});
+            }
+        }
+
+        if(this.state.email !== prevState.email){
+            let emailPattern = /(?!(^[.-].*|[^@]*[.-]@|.*\.{2,}.*)|^.{254}.)([a-zA-Z0-9!#$%&'*+\/=?^_`{|}~.-]+@)(?!-.*|.*-\.)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,15}/;
+            if(!this.state.email.match(emailPattern) && this.state.isEmailValid){
+                this.setState({isEmailValid: false});
+            }
+            else if(this.state.email.match(emailPattern) && !this.state.isEmailValid){
+                this.setState({isEmailValid: true});
+            }
+        }
     }
 
     render(){
@@ -30,12 +54,25 @@ export default class Register extends Component{
                 <main>
                     <h3>Register</h3>
                     <form noValidate={true} id="login">
-                        <input name="name" type="text" placeholder="Name" autoComplete="name" 
+                        <input name="name" type="text" placeholder="User Name" autoComplete="name" 
                             value={this.state.name} onChange={this.handleInputChange}
                         />
+                        {!this.state.isNameValid ?
+                            <div className="inputErrorMessage">
+                                User Name must only contain alphanumeric characters and _ <br/>
+                                User Name muct be between 4-20 characters long
+                            </div>
+                        : null}
+
                         <input name="email" type="text" placeholder="Email" autoComplete="email"
                             value={this.state.email} onChange={this.handleInputChange}
                         />
+                        {!this.state.isEmailValid ?
+                            <div className="inputErrorMessage">
+                                Invalid email
+                            </div>
+                        : null}
+
                         <input name="password" type="password" placeholder="Password" autoComplete="password"
                             value={this.state.password} onChange={this.handleInputChange}
                         />
