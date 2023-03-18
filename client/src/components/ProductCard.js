aecbcb931e981e3f18b778c0f2f0e0b1a8dba5db
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-03-18 18:05:09 +0000

    Main page, side menu and product card styles

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,9 +1,31 @@
-import React, {Component} from "react"
-import "../css/ProductCard.css";
-import { Link } from "react-router-dom"
+import React, {Component} from "react";
+import axios from "axios";
 
+import {SERVER_HOST} from "../config/global-constants";
+
+import {ReactComponent as NoImage} from "../icons/no-image.svg";
+
+import "../css/ProductCard.css";
 
 export default class ProductCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            imageData: ""
+        }
+    }
+
+    componentDidMount(){
+        if(this.props.product.images[0] !== null){
+            axios.get(`${SERVER_HOST}/products/image/${this.props.product.images[0]}`)
+                .then(res => {
+                    if(res.data){
+                        this.setState({imagesData: res.data.image});
+                    }
+                });
+        }
+    }
+
     render(){
         return(
             <a href={"/product/" + this.props.product._id}>
@@ -11,10 +33,16 @@ export default class ProductCard extends Component{
                 <div className="product-title">
                     <h3>{this.props.product.productName}</h3>
                 </div>
-                <img style={{width: "100%"}} src="https://preview.redd.it/80g7ddrm83n31.jpg?auto=webp&s=5e7190a2e6ff4fdd1861e8a1208a47277593fc70" alt=""/>
+
+                <div className="imageContainer">
+                    {this.state.imageData !== "" ?
+                        <img src={`data:;base64,${this.state.imageData}`} className="image"/>
+                    :
+                        <NoImage/>
+                    }
+                </div>
+                
                 <div className="product-info">
-                    <span className="product-desc">{this.props.product.description}</span>
-                    <span className="product-category">{this.props.product.category}</span>
                     <span className="product-price">€{this.props.product.productPrice}</span>
                     <span className="product-stock">Stock: {this.props.product.stock}</span>
                 </div>
