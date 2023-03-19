1c9f21ee39b93d99d98f4ed4c294653109f649c2
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-03-19 17:39:53 +0000

    Storing products in shopping bag in local storage

---
+++ client/src/components/ShoppingBag.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default class ShoppingBag extends Component{
+    constructor(props){
+        super(porps);
+        this.state = {
+            
+        }
+    }
+
+
+    render(){
+        return(
+            <div className="shoppingBag">
+                <Header/>
+                <main>
+
+                </main>
+                <Footer/>
+            </div>
+        );
+    }
+}
